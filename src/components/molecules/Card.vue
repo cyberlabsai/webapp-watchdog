37f79743ea1e5210d021a516dc4f08a6a1b02d4a
Gabriel Santiago Cardoso Rizzo <gabriel96.gsr@gmail.com>
2019-08-23 17:26:40 -0300

    Adding dirs

---
+++ src/components/molecules/Card.vue
@@ -0,0 +1,47 @@
+<template lang="pug">
+  include @/components/atoms/card.pug
+</template>
+
+<script>
+// import {
+//   mapGetters,
+//   mapActions
+// } from 'vuex'
+
+export default {
+  props: {
+    //  prop: {
+    //    type: String,
+    //    required: false,
+    //    validator (value) {
+    //      return value
+    //    },
+    //    default () {
+    //      return ''
+    //    }
+    //  }
+  },
+  data: () => ({}),
+  created () {
+    //  document.addEventListener('keyup', event => this.method(event))
+  },
+  mounted () {},
+  updated () {},
+  beforeDestroy () {
+    //  document.removeEventListener('keyup', event => this.method(event))
+  },
+  destroyed () {},
+  components: {},
+  computed: {
+    //   ...mapGetters([])
+  },
+  methods: {
+    //   ...mapActions([])
+  },
+  filters: {},
+  watch: {}
+}
+</script>
+
+<style lang="sass">
+</style>
